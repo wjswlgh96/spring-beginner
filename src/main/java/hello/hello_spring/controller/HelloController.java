0ac36c56435e06f5b377540f769c37449a1eaf04
JiHo Jeon <wjswlgh96@naver.com>
2024-12-18 18:01:07 +0900

    first commit

---
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -0,0 +1,17 @@
+package hello.hello_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+}
